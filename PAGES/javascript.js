4b352c86b61162f86205fe14f5b0e8dc451fc258
NVR04 <kleonis.dida@gmail.com>
2025-01-04 16:41:14 +0100

    Homepage V2

--- PAGES/javascript.js
+++ PAGES/javascript.js
@@ -1,4 +1,6 @@
 var title = document.getElementsByClassName("title");
+var background = document.getElementById("background");
+
 for (var i = 0; i < title.length; i++) {
     title[i].addEventListener("click", function(){
         window.location = "https://github.com/Nissyyy04/NVR-Editor";
@@ -12,4 +14,5 @@ githubButton.addEventListener("click", function(){
 var docsButton = document.getElementById("docsButton");
 docsButton.addEventListener("click", function(){
     window.location = "https://github.com/Nissyyy04/NVR-Editor/blob/main/README.md";
-});
+}); 
+
