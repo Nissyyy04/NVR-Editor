20c0e54a8f066525599ee0984f31f811a698548d
NVR04 <kleonis.dida@gmail.com>
2025-01-05 11:25:38 +0100

    Cleaned Up

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,11 +1,40 @@
 var title = document.getElementsByClassName("title");
 var background = document.getElementById("background");
+var innerHeaders = document.getElementsByClassName("innerHeader");
+
+const rootElement = document.documentElement;
+rootElement.style.setProperty('--cWidth', '100vw');
+rootElement.style.setProperty('--cHeight', '102vh');
+rootElement.style.setProperty('--maximized', '1');
 
 for (var i = 0; i < title.length; i++) {
     title[i].addEventListener("click", function(){
         window.location = "https://github.com/Nissyyy04/NVR-Editor";
     });
 }
+
+for (var i = 0; i < innerHeaders.length; i++) {
+    innerHeaders[i].addEventListener("click", function () {
+        const rootElement = document.documentElement;
+
+        // Retrieve the current value of `--maximized`
+        const maximized = getComputedStyle(rootElement).getPropertyValue('--maximized').trim();
+
+        // Toggle between states
+        if (maximized === '0') {
+            rootElement.style.setProperty('--cWidth', '100vw');
+            rootElement.style.setProperty('--cHeight', '102vh');
+            rootElement.style.setProperty('--maximized', '1');
+        } else if (maximized === '1') {
+            rootElement.style.setProperty('--cWidth', '70vw');
+            rootElement.style.setProperty('--cHeight', '70vh');
+            rootElement.style.setProperty('--maximized', '0');
+        } else {
+        }
+    });
+}
+
+
 var githubButton = document.getElementById("githubButton");
 githubButton.addEventListener("click", function(){
     window.location = "https://github.com/Nissyyy04/NVR-Editor";
